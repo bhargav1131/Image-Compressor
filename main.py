1d70f02b1ac833a7e40623b4befce5cf7f94b9c1
Bhargav Pratim Sharma <89238556+bhargav1131@users.noreply.github.com>
2022-10-23 21:49:33 +0530

    Updated !

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+Dated: 15/11/2021
+Aumthor: Bhargav Pratim Sharma
+"""
+
+"""
+Dated: 15/11/2021
+Aumthor: Bhargav Pratim Sharma
+"""
+
+from pickletools import optimize
+from tkinter import *
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+master = Tk()
+master.geometry("640x370")
+master.resizable(False, False)
+master.title("Thad Image Compressor")
+master.config(bg="black")
+qual = IntVar()
+qual.set(0)
+
+Label(text="\n\n\nDefine the quality in number.\nHigh value low compression, low value high compression: ", font=("arial", 12, "bold"), bg="black", fg="white").pack()
+Entry(master, textvariable=qual, bd=0, width=25, border=1).place(x=195, y=200)
+
+def compress():
+
+    file_path = askopenfilename()
+    Image = PIL.Image.open(file_path)
+    height, width = Image.size
+    Img = Image.resize((height,width), PIL.Image.Resampling.LANCZOS)
+    save_path = asksaveasfilename()
+
+    # qual = int(input("Define the quality in size. High value hlow compression, low value high compression: "))
+    Img.save(f"{save_path}" + ".jpg", optimize=True, quality=qual.get())
+
+Button(master, text="COMPRESS", command=compress,bd=0, bg="#EFF2F0"). place(x=260, y=250)
+
+master.mainloop()
---
+++ welcome_page.py
@@ -0,0 +1,21 @@
+from tkinter import *
+master = Tk()
+master.geometry("365x800")
+master.resizable(False, False)
+master.title("WELCOME")
+
+img = PhotoImage(file="/home/bhargav/My_projects/Image_compressor/images/background.png")
+label = Label(image=img)
+label.image = img
+label.pack()
+
+def nextPage():
+    master.destroy()
+    import toost
+
+# continue button
+continue_img = PhotoImage(file="/home/bhargav/My_projects/Image_compressor/images/Rectangle 1.png")
+continue1 = Button(master, image=continue_img,bd=0, command=nextPage, bg="white")
+continue1.place(x=150, y=720)
+
+master.mainloop()
